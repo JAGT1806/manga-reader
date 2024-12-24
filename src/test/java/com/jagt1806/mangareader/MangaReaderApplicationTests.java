f71bfb50603dd276692eab119f259ed73361bf05
JAGT <JAGT1806@gmail.com>
2024-12-24 16:57:35 -0500

    initial commit
    feat(.properties): uso de variables de entorno
    
    feat(.properties): conexión con la base de datos

---
+++ src/test/java/com/jagt1806/mangareader/MangaReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagt1806.mangareader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
