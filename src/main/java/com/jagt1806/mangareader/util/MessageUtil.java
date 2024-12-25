d7a849d2392715d53882f53f317ceb628368024a
JAGT <JAGT1806@gmail.com>
2024-12-24 19:44:49 -0500

    feat(util): creación de utilería para el sistema i18n

---
+++ src/main/java/com/jagt1806/mangareader/util/MessageUtil.java
@@ -0,0 +1,20 @@
+package com.jagt1806.mangareader.util;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class MessageUtil {
+    private MessageSource messageSource;
+
+    public String getMessage(String code) {
+        return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+    }
+
+    public String getMessage(String code, Object[] args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+    }
+}
